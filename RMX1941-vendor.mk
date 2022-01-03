053f926b7d08a876ffd0caa273f8561129ac28bb
SamarV-121 <samarvispute121@pm.me>
2022-01-03 18:22:50 +0530

    RMX1941-vendor: Enable MTK Engineering Mode
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- RMX1941-vendor.mk
+++ RMX1941-vendor.mk
@@ -11,6 +11,7 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX1941/proprietary/etc/firmware/hx_criteria.csv:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/hx_criteria.csv \
     vendor/realme/RMX1941/proprietary/etc/init/init.vtservice.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.vtservice.rc \
     vendor/realme/RMX1941/proprietary/lib/libcomutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libcomutils.so \
+    vendor/realme/RMX1941/proprietary/lib/libem_support_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libem_support_jni.so \
     vendor/realme/RMX1941/proprietary/lib/libimsma.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libimsma.so \
     vendor/realme/RMX1941/proprietary/lib/libimsma_adapt.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libimsma_adapt.so \
     vendor/realme/RMX1941/proprietary/lib/libimsma_rtp.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libimsma_rtp.so \
@@ -1430,5 +1431,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     EmCamera \
+    OppoEngineerMode \
     SensorHub \
     android.hardware.drm@1.3-service.widevine \

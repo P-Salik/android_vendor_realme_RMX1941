8be7afdb3f9daba0090639f86494011702725dae
Sarthak Roy <sarthakroy2002@gmail.com>
2022-04-18 07:48:23 +0000

    RMX1941-vendor: Nuke useless OPPO permissions
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Iae95ec6ed489a8d09c28cd91d5a168390b5bd953
    Signed-off-by: P Salik <peersalik16@gmail.com>

--- RMX1941-vendor.mk
+++ RMX1941-vendor.mk
@@ -505,7 +505,6 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX1941/proprietary/vendor/etc/mtk_omx_core.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/mtk_omx_core.cfg \
     vendor/realme/RMX1941/proprietary/vendor/etc/ncf_pack.ncf:$(TARGET_COPY_OUT_VENDOR)/etc/ncf_pack.ncf \
     vendor/realme/RMX1941/proprietary/vendor/etc/partition_permission.sh:$(TARGET_COPY_OUT_VENDOR)/etc/partition_permission.sh \
-    vendor/realme/RMX1941/proprietary/vendor/etc/permissions/vendor-oppo-hardware.cryptoeng.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor-oppo-hardware.cryptoeng.xml \
     vendor/realme/RMX1941/proprietary/vendor/etc/power_whitelist_cfg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/power_whitelist_cfg.xml \
     vendor/realme/RMX1941/proprietary/vendor/etc/powercontable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powercontable.xml \
     vendor/realme/RMX1941/proprietary/vendor/etc/powerscntbl.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerscntbl.xml \
